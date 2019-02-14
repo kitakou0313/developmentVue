28722158a0405c25e421a925a4b9b7f21a53c26e
Wiktor Klonowski <wklonowski@tsukaeru.net>
2019-02-14 23:27:53 +0900

    Add exercises

---
+++ src/views/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user">
+    <h2>User's details</h2>
+    <user-details/>
+    <!-- here will be user list -->
+  </div>
+</template>
+
+<script>
+import UserDetails from '@/components/UserDetails'
+
+import UserList from '@/assets/user-list.js'
+
+export default {
+  name: 'users',
+  components: {
+    UserDetails
+  },
+  data() {
+    return {
+      users: UserList
+    }
+  }
+}
+</script>
+
